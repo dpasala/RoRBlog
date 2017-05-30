d9e81ad199a1f61d52f31da8cb6af30bd057c631
dpasala <dpasala@gmail.com>
2017-05-30 17:41:26 +0530

    Add Route for BlogPost

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :blog_posts
+  resources :blog_posts, :path=>"blogposts"
+
   get 'home/index'
+  get 'home/newaction' => "blog_posts#index"
+
+  get ":title/p/:id" => "blog_posts#show", :id=> /[0-9]+/
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
